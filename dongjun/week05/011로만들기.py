bf4a85f1f4664d0c593d2584d14a9dc8203f6de4
김동준 <jkfdgd1114@naver.com>
2026-09-16 11:59:36 +0900

    1로만들기

---
+++ dongjun/week05/011로만들기.py
@@ -0,0 +1,11 @@
+x = int(input())
+arr = [0]*30001
+for i in range(2, x+1):
+    arr[i] = arr[i-1]+1
+    if i % 2 == 0:
+        arr[i] = min(arr[i], arr[i//2]+1)
+    if i % 3 == 0:
+        arr[i] = min(arr[i], arr[i//3]+1)
+    if i % 5 == 0:
+        arr[i] = min(arr[i], arr[i//5]+1)
+print(arr[x])
